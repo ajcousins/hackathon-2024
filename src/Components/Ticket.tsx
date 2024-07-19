9beac13a3e2d7c048f3077a2f4e936d020c0d83e
Mari Lovell <mari.lovell@roh.org.uk>
2024-07-19 14:36:54 +0100

    countdown to event

--- src/Components/Ticket.tsx
+++ src/Components/Ticket.tsx
@@ -1,4 +1,4 @@
-import React from 'react'; 
+import React, { useEffect, useState } from 'react'; 
 import '@radix-ui/themes/styles.css';
 import { Theme, Button } from '@radix-ui/themes';
 import { data } from '../assets/libs/mock-data';
@@ -6,6 +6,30 @@ import { ArrowRightIcon } from '@radix-ui/react-icons';
 import qrcode from '../assets/images/qr_example.png'
 
 function Ticket() {
+
+    const [countdownTime, setCountdownTime] = useState(0);
+
+    useEffect(() => {
+      // Calculate the initial countdown time
+      const currentTime = Date.now();
+      let countdown = data.attributes.date - currentTime;
+    
+      // Update the countdown state every second
+      const intervalId = setInterval(() => {
+        if (countdown > 0) {
+          setCountdownTime(countdown);
+          countdown -= 1000; // Decrease the countdown by 1 second
+        } else {
+          clearInterval(intervalId); // Stop the interval when countdown reaches 0
+        }
+      }, 1000);
+    
+      return () => {
+        clearInterval(intervalId); // Cleanup function to stop the interval when the component unmounts
+      };
+    }, []);
+    
+
   
     return (
       <Theme accentColor='red' grayColor='sand' radius='large' scaling='95%'>
@@ -13,23 +37,13 @@ function Ticket() {
             <div className='ticket'>
               <h1>{data.attributes.title}</h1>
               <div>
+                <p>7pm</p><p>18/07/24</p>
+              </div>
+              <div><p>{countdownTime}</p></div>
+              <div>
               <img src={qrcode}  alt="qr code for your ticket" />
               </div>
             </div>
-            <div className='wayfinder-text'>
-              <h1>1.</h1>
-              <ul>
-                <li>Staircase...</li>
-                <li>Lift XYZ...</li>
-              </ul>
-              <p>Stuff</p>
-            </div>
-          <div className='bottom-nav'>
-            <Button>
-              Find My Seat
-              <ArrowRightIcon />
-            </Button>
-          </div>
         </div>
       </Theme>
     );
